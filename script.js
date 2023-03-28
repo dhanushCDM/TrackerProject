6eba3451eac7dc428c14da164902e1fb25e8416c
dhanushCDM <129148703+dhanushCDM@users.noreply.github.com>
2023-03-28 14:25:26 +0530

    display Date and Day in the page header

--- script.js
+++ script.js
@@ -188,16 +188,16 @@ const controlKeyPressed = event.ctrlKey || event.metaKey;
 
 function displayCurrentDayDate() {
   const now = new Date();
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const day = days[now.getDay()];
-  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  const month = monthNames[now.getMonth()];
-  const date = now.getDate();
-  const year = now.getFullYear();
-  const formattedDate = `${date}-${month}-${year}`;
-
-  document.getElementById('currentDayDate').innerText = `${day}, ${formattedDate}`;
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long'
+  }).format(now);
+  
+  document.getElementById('currentDayDate').innerText = formattedDate;
 }
+
 
 function resetActivities() {
   if (confirm('Are you sure you want to reset all activities? This action cannot be undone.')) {
